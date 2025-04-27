3aa2e40806c27bc88b4d3cac37f2e7c3e0a1cc6b
Léa BAR <contact@0xlea.fr>
2025-04-27 18:53:37 +0200

    [Add] Add background

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import styles from "./layout.module.css";
 
 export const metadata: Metadata = {
   title: "WhoSaidThat",
   description: "WhoSaidThat is a multiplayer game where each player submits a quote about another player, and everyone must guess who said each quote.",
 };
+
+
 
 export default function RootLayout({
   children,
@@ -13,6 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+          <div className={styles.animatedBackground}>
+            <div className={styles.stripeContainer}></div>
+            <div className={styles.overlay}></div>
+          </div>
         {children}
       </body>
     </html>
