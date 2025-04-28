0b3fdcf9b37572d0bfcf1c8ea4d9fa16a9f9fce8
Léa BAR <contact@0xlea.fr>
2025-04-28 09:37:29 +0200

    [Add] Add logo and responsive

--- src/components/mainmenu/mainmenu.tsx
+++ src/components/mainmenu/mainmenu.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import styles from "./mainmenu.module.css";
 
 export default function MainMenu(){
     return (
         <div className={styles.menuContainer}>
+            <Image width={600} height={400} src="/assets/img/logo.png" className={styles.logo} alt="Logo"/>
             <div className={styles.codeContainer}>
                 <input type="text" id="code" placeholder="Code" className={styles.code} maxLength={6}></input>
             </div>
